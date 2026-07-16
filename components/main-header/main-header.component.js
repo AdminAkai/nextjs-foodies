6d457252a2f857b8593607c3f5d4c64e1bf985be
AdminAkai <akatoirotech@gmail.com>
2026-07-16 17:16:52 -0400

    Moved MainHeaderBackground to MainHeader component

--- components/main-header/main-header.component.js
+++ components/main-header/main-header.component.js
@@ -2,18 +2,22 @@ import Link from "next/link";
 import Image from "next/image";
 
 import NavHeader from "@/components/nav-header";
+import MainHeaderBackground from "@/components/main-header-background"
 import logoImg from '@/assets/logo.png'
 
 import styles from "./main-header.module.css"
 
 export default function MainHeader() {
   return (
-    <header className={styles.header}>
-      <Link className={styles.logo} href="/">
-        <Image src={logoImg} alt="A plate with food on it" priority />
-        NextLevel Food
-      </Link>
-      <NavHeader />
-    </header>
+    <>
+      <MainHeaderBackground />
+      <header className={styles.header}>
+        <Link className={styles.logo} href="/">
+          <Image src={logoImg} alt="A plate with food on it" priority />
+          NextLevel Food
+        </Link>
+        <NavHeader />
+      </header>
+    </>
   )
 }
