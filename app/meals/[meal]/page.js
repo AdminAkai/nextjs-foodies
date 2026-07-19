846a0c5349a2c7b904e31ed89f7bc0132f24446a
AdminAkai <akatoirotech@gmail.com>
2026-07-18 21:38:56 -0400

    Added meal details

--- app/meals/[meal]/page.js
+++ app/meals/[meal]/page.js
@@ -1,7 +1,32 @@
+import Image from 'next/image'
+
+import styles from './page.module.css'
+import { getMeal } from '@/lib/meals.lib'
+
 export default function MealPage({ params }) {
+  // the folder name for the subroute is the param here, meal = id of the meal
+  const { title, creator, image, creator_email, summary, instructions } = getMeal(params.meal)
+
+  // newlines are being ignored, replace with linebreaks
+  const formatInstructions = instructions.replace(/\n/g, '<br />');
+
   return (
-    <main>
-      <h1>{params.meal}</h1>
-    </main>
+    <>
+      <header className={styles.header}>
+        <div className={styles.image}>
+          <Image src={image} fill alt="Image of the associated meal" />
+        </div>
+        <div className={styles.headerText}>
+          <h1>{title}</h1>
+          <p className={styles.creator}>
+            by <a href={`mailto:${creator_email}`}>{creator}</a>
+          </p>
+          <p className={styles.summary}>{summary}</p>
+        </div>
+      </header>
+      <main>
+        <p className={styles.instructions} dangerouslySetInnerHTML={{ __html: formatInstructions }}></p>
+      </main>
+    </>
   )
 }
