55ab907c447af52fffe5e8b4f86a7cfe88f97f75
AdminAkai <akatoirotech@gmail.com>
2026-07-19 00:46:29 -0400

    Added image picker and preview to share meal page

---
+++ components/image-picker/image-picker.component.js
@@ -0,0 +1,61 @@
+'use client';
+
+import { useRef, useState } from 'react';
+
+import Image from 'next/image';
+
+import styles from './image-picker.module.css'
+
+export default function ImagePicker({ label, name }) {
+  const imageInputRef = useRef(null);
+
+  const [selectedImage, setSelectedImage] = useState(null);
+
+  const handlePickImage = () => {
+    imageInputRef.current.click();
+  }
+
+  const handleImageChange = (e) => {
+    const file = e.target.files[0];
+
+    if (!file) {
+      setSelectedImage(null);
+      return;
+    };
+
+    const fileReader = new FileReader();
+
+    fileReader.onload = () => {
+      setSelectedImage(fileReader.result)
+    }
+
+    fileReader.readAsDataURL(file);
+  }
+
+  return (
+    <div className={styles.picker}>
+      <label htmlFor={name}>{label}</label>
+      <div className={styles.controls}>
+        <div className={styles.preview}>
+          {selectedImage 
+            ? 
+              <Image src={selectedImage} alt="User selected image preview" fill />
+            :
+              <p>No image selected.</p>
+          }
+        </div>
+        <input 
+          type="file" id={name} 
+          accept="image/png, image/jpeg" 
+          name={name} 
+          className={styles.input}
+          ref={imageInputRef}
+          onChange={handleImageChange}
+        />
+        <button className={styles.button} type="button" onClick={handlePickImage}>
+          Pick an Image
+        </button>
+      </div>
+    </div>
+  )
+}
